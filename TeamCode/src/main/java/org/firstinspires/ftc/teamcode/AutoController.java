4344e312683a9395f7c7f25f189d70cdb12f1c0d
mageowl <32573897+mageowl@users.noreply.github.com>
2025-09-18 15:18:02 -0700

    Add auto and teleop classes.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoController.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@Autonomous(name = "Auto Controller v0.1", group = "team code")
+public class AutoController extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
